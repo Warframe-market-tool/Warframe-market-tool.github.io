8933e0af1d235fc96b589bb0d94df81dbbb68dac
Soutif Thomas <th.soutif@gmail.com>
2024-07-31 00:36:44 +0200

    take the  last day if current day stat not available

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-function getCurrentFormattedDate() {
+function getCurrentFormattedDate(offsetDays = 0) {
     const date = new Date();
+    date.setDate(date.getDate() + offsetDays);
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
     const year = date.getFullYear();
@@ -41,4 +42,40 @@ function loadCSV(filePath) {
             $('#csvTable').DataTable();
         }
     });
+}
+
+function fileExists(url, callback) {
+    $.ajax({
+        url: url,
+        type: 'HEAD',
+        error: function() {
+            callback(false);
+        },
+        success: function() {
+            callback(true);
+        }
+    });
+}
+
+function loadCSVForDate(offsetDays = 0) {
+    const date = getCurrentFormattedDate(offsetDays);
+    const filePath = `/csv/stats_${date}.csv`;
+    fileExists(filePath, function(exists) {
+        if (exists) {
+            $("#today-date").append(date);
+            loadCSV(filePath);
+        } else if (offsetDays === 0) {
+            // Try loading the file for the previous day if today's file doesn't exist
+            loadCSVForDate(-1);
+        } else {
+            // If previous day's file also doesn't exist
+            $('#csvHeader').html('<th>No data available</th>');
+            $('#csvBody').html('<tr><td>No data available</td></tr>');
+        }
+    });
+}
+
+function initializePage() {
+    // Attempt to load the CSV file for the current date
+    loadCSVForDate(0);
 }
