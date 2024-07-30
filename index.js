d49d3cb54e1bf63db10df5ffc304671c088db3d1
Soutif Thomas <th.soutif@gmail.com>
2024-07-30 19:47:47 +0200

    refactor the code

--- index.js
+++ index.js
@@ -5,3 +5,40 @@ function getCurrentFormattedDate() {
     const year = date.getFullYear();
     return `${day}-${month}-${year}`;
 }
+
+function loadCSV(filePath) {
+    Papa.parse(filePath, {
+        download: true,
+        header: true,
+        complete: function(results) {
+            // Get data
+            var data = results.data;
+            
+            // Clear the existing table
+            $('#csvHeader').empty();
+            $('#csvBody').empty();
+
+            // Generate table header
+            var headerHtml = '';
+            var keys = Object.keys(data[0]);
+            keys.forEach(function(key) {
+                headerHtml += '<th>' + key + '</th>';
+            });
+            $('#csvHeader').html(headerHtml);
+
+            // Generate table body
+            var bodyHtml = '';
+            data.forEach(function(row) {
+                bodyHtml += '<tr>';
+                keys.forEach(function(key) {
+                    bodyHtml += '<td>' + row[key] + '</td>';
+                });
+                bodyHtml += '</tr>';
+            });
+            $('#csvBody').html(bodyHtml);
+
+            // Initialize DataTable
+            $('#csvTable').DataTable();
+        }
+    });
+}
